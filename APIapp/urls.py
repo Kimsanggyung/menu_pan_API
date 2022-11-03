41c38d4cb1c2c39b9396bc61166915baf5d66624
Kim sanggyung <ksang0906@gmail.com>
2022-11-03 11:14:01 +0900

    로그인 관련API url설정

--- APIapp/urls.py
+++ APIapp/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import MenuList, MenuDetail
+from .views import SignupAPI, LoginAPI, LogoutAPI
 
 urlpatterns = [
   path('menu/', MenuList.as_view()),
   path('menu/<int:pk>/', MenuDetail.as_view()),
+  path('signup/', SignupAPI.as_view()),
+  path('login/', LoginAPI.as_view()),
+  path('logout/', LogoutAPI.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
